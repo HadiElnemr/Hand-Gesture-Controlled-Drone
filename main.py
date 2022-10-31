51051bd7763e7eeae9c888040a42cf98eefa522a
HadiElnemr <hadi.elnemr@gmail.com>
2022-10-31 23:59:34 +0200

    remove black gloves detection and detect based on normal hands

--- main.py
+++ main.py
@@ -17,26 +17,32 @@ while True:
     # Get image frame
     success, frame = cap.read()
     
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # gray = 255-gray
     # find the colors within the specified BGR colour boundaries and apply the mask
-    mask = cv2.inRange(img, lower, upper)
-    output = cv2.bitwise_and(img, img, mask = mask)
+    # mask = cv2.inRange(frame, lower, upper)
+    
+    # cv2.imshow('mask', mask)
+    # cv2.imshow('255-mask', 255-mask)
 
+    # output = cv2.bitwise_and(frame, frame, mask = 255 - mask)
+    # cv2.imshow('output', output)
     # take only that specific colour
-    # img = np.hstack([img, output])
-    img = output
+    # frame = np.hstack([frame, output])
+    # frame = output
 
     # Find the hand and its landmarks
-    hands, img = detector.findHands(img)  # with draw
-    # hands = detector.findHands(img, draw=False)  # without draw
-    
+    hands, frame_draw_hand = detector.findHands(deepcopy(frame))  # with draw
+    # hands = detector.findHands(frame, draw=False)  # without draw
+
     
     if hands:
         # Hand 1
         hand1 = hands[0]
-        lmList1 = hand1["lmList"]  # List of 21 Landmark points
-        bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
-        centerPoint1 = hand1['center']  # center of the hand cx,cy
-        handType1 = hand1["type"]  # Handtype Left or Right
+        # lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        # bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+        # centerPoint1 = hand1['center']  # center of the hand cx,cy
+        # handType1 = hand1["type"]  # Handtype Left or Right
 
         fingers1 = detector.fingersUp(hand1)
         if fingers1 == up:
