d844a52c5ba5896729c11317999d577589ca95f2
HadiElnemr <hadi.elnemr@gmail.com>
2022-11-01 08:31:08 +0200

    add try except in communication with esp for testing without connection

--- util.py
+++ util.py
@@ -1,7 +1,8 @@
 import serial
 import time
-
-esp32 = serial.Serial(port='COM5', baudrate=115200, timeout=.1)
-
+try:
+    esp32 = serial.Serial(port='COM5', baudrate=115200, timeout=.1)
+except:
+    pass
 def write_esp(x):
     esp32.write(bytes(x, 'utf-8'))
