ca15d6460df6d45922a65861dfb51f763f4c1ab3
HadiElnemr <hadi.elnemr@gmail.com>
2022-11-04 14:04:26 +0200

    add python UDP server to connect to esp through wifi

---
+++ server.py
@@ -0,0 +1,31 @@
+import socket
+
+localIP     = "127.0.0.1"
+localPort   = 20001
+bufferSize  = 1024
+
+msgFromServer       = "Hello UDP Client"
+bytesToSend         = str.encode(msgFromServer)
+
+# Create a datagram socket
+UDPServerSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+UDPServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+# Bind to address and ip
+UDPServerSocket.bind(("", localPort)) 
+
+print("UDP server up and listening")
+
+# Listen for incoming datagrams
+def write_esp(x):
+
+    #bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    #message = bytesAddressPair[0]
+    #address = bytesAddressPair[1]
+    #clientMsg = "Message from Client:{}".format(message)
+    #clientIP  = "Client IP Address:{}".format(address)
+    #print(clientMsg)
+    #print(clientIP)
+    # Sending a reply to client
+    UDPServerSocket.sendto(bytes(x, "utf-8"), ("<broadcast>", 4210))
