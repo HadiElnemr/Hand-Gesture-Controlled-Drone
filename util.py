2cc2f0cd941db7de7e67624f5f566487106aac9b
HadiElnemr <hadi.elnemr@gmail.com>
2022-11-04 14:02:45 +0200

    remove serial esp writing

--- util.py
+++ util.py
@@ -2,11 +2,7 @@ import serial
 import time
 # try:
 esp32 = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=.1)
-# except:
-    # print('Connection with esp not found')
 
 def write_esp(x):
-    # try:
-    esp32.write(bytes(str(x), 'utf-8'))
-    # except:
-    # print('Connection not found')
+    #esp32.write(bytes(str(x), 'utf-8'))
+    
